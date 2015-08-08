a14d87cedd7e1590e482aa46829a96e44f6e1662
dengoswei <dengoswei@gmail.com>
2015-08-08 17:09:45 +0800

    1. refactor; 2. add test case;

---
+++ spaxos_instance.go
@@ -0,0 +1,274 @@
+package spaxos
+
+import (
+	pb "spaxos/spaxospb"
+)
+
+type stepFunc func(sp *spaxos, msg pb.Message)
+
+type spaxosInstance struct {
+	chosen bool
+	index  uint64
+	// proposer
+	maxProposedNum     uint64
+	maxAcceptedHintNum uint64
+	proposingValue     []byte
+	rspVotes           map[uint64]bool
+	stepProposer       stepFunc
+
+	// acceptor
+	promisedNum   uint64
+	acceptedNum   uint64
+	acceptedValue []byte
+	// stepAccpt     stepSpaxosFunc
+}
+
+func newSpaxosInstance(index uint64) *spaxosInstance {
+	return &spaxosInstance{index: index}
+}
+
+func rebuildSpaxosInstance(hs pb.HardState) *spaxosInstance {
+	ins := spaxosInstance{
+		chosen: hs.Chosen, index: hs.Index,
+		maxProposedNum: hs.MaxProposedNum,
+		promisedNum:    hs.MaxPromisedNum,
+		acceptedNum:    hs.MaxAcceptedNum,
+		acceptedValue:  hs.AcceptedValue}
+	return &ins
+}
+
+func (ins *spaxosInstance) getHardState() pb.HardState {
+	return pb.HardState{
+		Chosen:         ins.chosen,
+		Index:          ins.index,
+		MaxProposedNum: ins.maxProposedNum,
+		MaxPromisedNum: ins.promisedNum,
+		MaxAcceptedNum: ins.acceptedNum,
+		AcceptedValue:  ins.acceptedValue}
+}
+
+func getDefaultRspMsg(msg pb.Message) pb.Message {
+	return pb.Message{Reject: false,
+		Index: msg.Index, From: msg.To, To: msg.From,
+		Entry: pb.PaxosEntry{PropNum: msg.Entry.PropNum}}
+}
+
+// acceptor
+func (ins *spaxosInstance) updatePromised(msg pb.Message) pb.Message {
+	rsp := getDefaultRspMsg(msg)
+	rsp.Type = pb.MsgPropResp
+	if ins.promisedNum > msg.Entry.PropNum {
+		rsp.Reject = true
+		return rsp
+	}
+
+	if nil != ins.acceptedValue {
+		rsp.Entry.AccptNum = ins.acceptedNum
+		rsp.Entry.Value = ins.acceptedValue
+	}
+
+	ins.promisedNum = msg.Entry.PropNum
+	return rsp
+}
+
+func (ins *spaxosInstance) updateAccepted(msg pb.Message) pb.Message {
+	rsp := getDefaultRspMsg(msg)
+	rsp.Type = pb.MsgAccptResp
+	if ins.promisedNum > msg.Entry.PropNum {
+		rsp.Reject = true
+		return rsp
+	}
+
+	assert(nil != msg.Entry.Value)
+	ins.promisedNum = msg.Entry.PropNum
+	ins.acceptedNum = msg.Entry.PropNum
+	ins.acceptedValue = msg.Entry.Value
+	return rsp
+}
+
+func (ins *spaxosInstance) stepAcceptor(sp *spaxos, msg pb.Message) {
+	assert(nil != sp)
+	assert(ins.index == msg.Index)
+
+	var rsp pb.Message
+	switch msg.Type {
+	case pb.MsgPropResp:
+		rsp = ins.updatePromised(msg)
+	case pb.MsgAccptResp:
+		rsp = ins.updateAccepted(msg)
+	default:
+		assert(false)
+	}
+
+	sp.appendMsg(rsp)
+	if false == rsp.Reject {
+		sp.appendHardState(ins.getHardState())
+	}
+}
+
+// proposer
+func (ins *spaxosInstance) Propose(sp *spaxos, proposingValue []byte) {
+	assert(nil != sp)
+	assert(nil != proposingValue)
+	if false == ins.chosen {
+		ins.proposingValue = proposingValue
+	}
+
+	ins.beginPreparePhase(sp)
+}
+
+func (ins *spaxosInstance) beginPreparePhase(sp *spaxos) {
+	assert(nil != sp)
+	if ins.chosen {
+		ins.markChosen(sp, false)
+		return
+	}
+
+	// inc ins.maxProposedNum
+	nextProposeNum := sp.getNextProposeNum(
+		ins.maxProposedNum, ins.promisedNum)
+
+	req := pb.Message{
+		Type: pb.MsgProp, Index: ins.index, From: sp.id,
+		Entry: pb.PaxosEntry{PropNum: nextProposeNum}}
+
+	rsp := ins.updatePromised(req)
+	assert(false == rsp.Reject)
+	assert(rsp.From == req.From)
+
+	ins.rspVotes = make(map[uint64]bool)
+	ins.rspVotes[sp.id] = true
+	ins.maxProposedNum = nextProposeNum
+	ins.stepProposer = ins.stepPrepareRsp
+
+	sp.appendMsg(req)
+	sp.appendHardState(ins.getHardState())
+}
+
+func (ins *spaxosInstance) beginAcceptPhase(sp *spaxos) {
+	assert(nil != sp)
+	// TODO:
+	// accepted action when ins is chosen ?
+	req := pb.Message{
+		Type: pb.MsgAccpt, Index: ins.index, From: sp.id,
+		Entry: pb.PaxosEntry{
+			PropNum: ins.maxProposedNum, Value: ins.proposingValue}}
+
+	rsp := ins.updateAccepted(req)
+	if true == rsp.Reject {
+		// reject by self
+		// => backoff to beginPreparePhase
+		// NOTE: this may cause live lock
+		ins.beginPreparePhase(sp)
+		return
+	}
+
+	assert(false == rsp.Reject)
+	assert(rsp.From == req.From)
+
+	ins.rspVotes = make(map[uint64]bool)
+	ins.rspVotes[sp.id] = true
+	ins.stepProposer = ins.stepAcceptRsp
+
+	sp.appendMsg(req)
+	sp.appendHardState(ins.getHardState())
+}
+
+func (ins *spaxosInstance) markChosen(sp *spaxos, broadcast bool) {
+	assert(nil != sp)
+
+	ins.chosen = true
+	sp.submitChosen(ins)
+	ins.stepProposer = ins.stepChosen
+	if broadcast {
+		req := pb.Message{
+			Type: pb.MsgChosen, Index: ins.index, From: sp.id,
+			Entry: pb.PaxosEntry{Value: ins.acceptedValue}}
+		sp.appendMsg(req)
+	}
+}
+
+func (ins *spaxosInstance) stepPrepareRsp(
+	sp *spaxos, msg pb.Message) {
+	assert(nil != sp)
+	assert(ins.index == msg.Index)
+
+	// TODO
+	// only deal with MsgPropResp msg;
+	if pb.MsgPropResp != msg.Type ||
+		ins.maxProposedNum != msg.Entry.PropNum {
+		return // ignore the mismatch prop num msg
+	}
+
+	if val, ok := ins.rspVotes[msg.From]; ok {
+		// inconsist !
+		assert(val == msg.Reject)
+		return
+	}
+
+	ins.rspVotes[msg.From] = !msg.Reject
+	if false == msg.Reject {
+		// update the maxAcceptedHitNum & proposingValue
+		if ins.maxAcceptedHintNum < msg.Entry.AccptNum {
+			ins.maxAcceptedHintNum = msg.Entry.AccptNum
+			ins.proposingValue = msg.Entry.Value
+			assert(nil != ins.proposingValue)
+		}
+	}
+
+	if promisedByMajority(sp, ins.rspVotes) {
+		ins.beginAcceptPhase(sp)
+	} else if rejectedByMajority(sp, ins.rspVotes) {
+		ins.beginPreparePhase(sp)
+	}
+}
+
+func (ins *spaxosInstance) stepAcceptRsp(sp *spaxos, msg pb.Message) {
+	assert(nil != sp)
+	assert(ins.index == msg.Index)
+
+	// TODO
+	// only deal with MsgAccptResp msg;
+	if pb.MsgAccptResp != msg.Type ||
+		ins.maxProposedNum != msg.Entry.PropNum {
+		return // ignore the mismatch prop num msg
+	}
+
+	if val, ok := ins.rspVotes[msg.From]; ok {
+		// inconsist !
+		assert(val == msg.Reject)
+		return
+	}
+
+	ins.rspVotes[msg.From] = !msg.Reject
+	if acceptedByMajority(sp, ins.rspVotes) {
+		ins.markChosen(sp, true)
+	} else if rejectedByMajority(sp, ins.rspVotes) {
+		ins.beginPreparePhase(sp)
+	}
+}
+
+func (ins *spaxosInstance) stepChosen(sp *spaxos, msg pb.Message) {
+	assert(nil != sp)
+	assert(ins.index == msg.Index)
+	assert(true == ins.chosen)
+
+	switch msg.Type {
+	// TODO
+	}
+}
+
+func promisedByMajority(sp *spaxos, rspVotes map[uint64]bool) bool {
+	assert(nil != sp)
+	return sp.asMajority(rspVotes, true)
+}
+
+func acceptedByMajority(sp *spaxos, rspVotes map[uint64]bool) bool {
+	assert(nil != sp)
+	return sp.asMajority(rspVotes, true)
+}
+
+func rejectedByMajority(sp *spaxos, rspVotes map[uint64]bool) bool {
+	assert(nil != sp)
+	return sp.asMajority(rspVotes, false)
+}
---
+++ spaxos_instance_test.go
@@ -0,0 +1,167 @@
+package spaxos
+
+import (
+	"bytes"
+	"math/rand"
+	"testing"
+
+	pb "spaxos/spaxospb"
+)
+
+func TestNewSpaxosInstance(t *testing.T) {
+	PrintIndicate()
+
+	index := uint64(rand.Uint32())
+	ins := newSpaxosInstance(index)
+	assert(nil != ins)
+	assert(index == ins.index)
+	assert(false == ins.chosen)
+	assert(0 == ins.maxProposedNum)
+	assert(0 == ins.maxAcceptedHintNum)
+	assert(nil == ins.proposingValue)
+	assert(nil == ins.rspVotes)
+	assert(nil == ins.stepProposer)
+	assert(0 == ins.promisedNum)
+	assert(0 == ins.acceptedNum)
+	assert(nil == ins.acceptedValue)
+}
+
+func TestGetHardState(t *testing.T) {
+	PrintIndicate()
+
+	ins := RandSpaxosInstance()
+	assert(nil != ins)
+
+	hs := ins.getHardState()
+	assert(hs.Chosen == ins.chosen)
+	assert(hs.Index == ins.index)
+	assert(hs.MaxProposedNum == ins.maxProposedNum)
+	assert(hs.MaxPromisedNum == ins.promisedNum)
+	assert(hs.MaxAcceptedNum == ins.acceptedNum)
+	assert(0 == bytes.Compare(hs.AcceptedValue, ins.acceptedValue))
+}
+
+func TestRebuildSpaxosInstance(t *testing.T) {
+	PrintIndicate()
+
+	ins := RandSpaxosInstance()
+	assert(nil != ins)
+
+	hs := ins.getHardState()
+
+	newins := rebuildSpaxosInstance(hs)
+	assert(nil != newins)
+	assert(ins.index == newins.index)
+	assert(ins.maxProposedNum == newins.maxProposedNum)
+	assert(ins.promisedNum == newins.promisedNum)
+	assert(ins.acceptedNum == newins.acceptedNum)
+	assert(0 == bytes.Compare(ins.acceptedValue, newins.acceptedValue))
+}
+
+// test accepted
+func TestUpdatePromised(t *testing.T) {
+	PrintIndicate()
+
+	ins := RandSpaxosInstance()
+
+	msg := pb.Message{
+		Type: pb.MsgProp, To: 1, From: 2,
+		Index: ins.index,
+		Entry: pb.PaxosEntry{PropNum: ins.maxProposedNum}}
+
+	// case 1: reject
+	ins.promisedNum = ins.maxProposedNum + 1
+	rejectRsp := ins.updatePromised(msg)
+	assert(pb.MsgPropResp == rejectRsp.Type)
+	assert(rejectRsp.Index == ins.index)
+	assert(rejectRsp.From == msg.To)
+	assert(rejectRsp.To == msg.From)
+	assert(true == rejectRsp.Reject)
+	assert(rejectRsp.Entry.PropNum == msg.Entry.PropNum)
+
+	// case 2: promised with accepted value
+	ins.promisedNum = ins.maxProposedNum - 1
+	assert(nil != ins.acceptedValue)
+	rsp := ins.updatePromised(msg)
+	assert(pb.MsgPropResp == rsp.Type)
+	assert(rsp.Index == ins.index)
+	assert(false == rsp.Reject)
+	assert(rsp.Entry.PropNum == msg.Entry.PropNum)
+	assert(rsp.Entry.AccptNum == ins.acceptedNum)
+	assert(0 == bytes.Compare(rsp.Entry.Value, ins.acceptedValue))
+	assert(ins.promisedNum == msg.Entry.PropNum)
+
+	// case 3: promised with nil value
+	ins.promisedNum = ins.maxProposedNum - 1
+	ins.acceptedValue = nil
+	ins.acceptedNum = 0
+	rsp = ins.updatePromised(msg)
+	assert(rsp.Index == ins.index)
+	assert(false == rsp.Reject)
+	assert(rsp.Entry.PropNum == msg.Entry.PropNum)
+	assert(0 == rsp.Entry.AccptNum)
+	assert(nil == rsp.Entry.Value)
+	assert(ins.promisedNum == msg.Entry.PropNum)
+}
+
+func TestupdateAccepted(t *testing.T) {
+	PrintIndicate()
+
+	ins := RandSpaxosInstance()
+
+	propValue := RandByte(100)
+
+	msg := pb.Message{
+		Type: pb.MsgAccpt, To: 1, From: 2,
+		Index: ins.index,
+		Entry: pb.PaxosEntry{
+			PropNum: ins.maxProposedNum, Value: propValue}}
+	// case 1: reject
+	ins.promisedNum = ins.maxProposedNum + 1
+	rejectRsp := ins.updateAccepted(msg)
+	assert(pb.MsgAccptResp == rejectRsp.Type)
+	assert(rejectRsp.Index == msg.Index)
+	assert(rejectRsp.From == msg.To)
+	assert(rejectRsp.To == msg.From)
+	assert(true == rejectRsp.Reject)
+	assert(rejectRsp.Entry.PropNum == msg.Entry.PropNum)
+
+	// case 2: accepted
+	ins.promisedNum = ins.maxProposedNum
+	rsp := ins.updateAccepted(msg)
+	assert(pb.MsgAccptResp == rsp.Type)
+	assert(rsp.Index == msg.Index)
+	assert(false == rsp.Reject)
+	assert(rsp.From == msg.To)
+	assert(rsp.To == msg.From)
+	assert(rsp.Entry.PropNum == msg.Entry.PropNum)
+	assert(0 == bytes.Compare(msg.Entry.Value, ins.acceptedValue))
+	assert(ins.promisedNum == msg.Entry.PropNum)
+	assert(ins.acceptedNum == msg.Entry.AccptNum)
+}
+
+func TestStepAcceptor(t *testing.T) {
+
+}
+
+// test proposer
+func TestRspVotes(t *testing.T) {
+	PrintIndicate()
+
+	sp := RandSpaxos()
+	assert(nil != sp)
+
+	cnt := uint64(len(sp.groups))
+	// case 1: promisedByMaority
+	assert(1 <= cnt)
+	var ok bool
+	ok = promisedByMajority(sp, RandRspVotes(0, cnt/2+1))
+	assert(true == ok)
+	ok = promisedByMajority(sp, RandRspVotes(0, cnt/2))
+	assert(false == ok)
+
+	ok = rejectedByMajority(sp, RandRspVotes(cnt/2, 0))
+	assert(false == ok)
+	ok = rejectedByMajority(sp, RandRspVotes(cnt/2+1, 0))
+	assert(true == ok)
+}
